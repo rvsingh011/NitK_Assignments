f7387ea8818654ba67f6f0b925c9c156feb016b1
Ravi Singh <rvsingh011@gmail.com>
2017-08-13 19:58:51 +0530

    GitIgnore Updated

--- Algorithm/fabonacci.py
+++ Algorithm/fabonacci.py
@@ -1,12 +1,15 @@
+from functools import lru_cache
+
+
 class Fibonacci:
 
-    fibonacci_cache = []
+    fibonacci_cache = {}
 
     def fibonacci_value(self, n):
         """This is function to print Fibonacci value of a number at Given position
             This function is coded in python 3.5. """
         # check for integer
-        if type(n) != "int":
+        if not isinstance(n, int):
             raise TypeError("Please only enter integer")
         if n <= 0:
             raise ValueError("Kindly Enter positive integer only ")
@@ -20,9 +23,8 @@ class Fibonacci:
 
     def fibonacci_iter(self, n):
         """This function print the fabo series till the given integer"""
-
         # check for integer
-        if type(n) != "int":
+        if not isinstance(n, int):
             raise TypeError("Please only enter integer")
         if n <= 0:
             raise ValueError("Kindly Enter positive integer only ")
@@ -45,13 +47,14 @@ class Fibonacci:
 
     def fabonacci_explicit_momization(self, n):
         # check for integer
-        if type(n) != "int":
+        if not isinstance(n, int):
             raise TypeError("Please only enter integer")
         if n <= 0:
             raise ValueError("Kindly Enter positive integer only ")
 
-        # Reusing value value from Cache
+        # # Reusing value value from Cache
         if n in self.fibonacci_cache:
+            print(self.fibonacci_cache[n])
             return self.fibonacci_cache[n]
 
         if n == 1:
@@ -62,6 +65,17 @@ class Fibonacci:
             value = self.fibonacci_value(n - 1) + self.fibonacci_value(n - 2)
             self.fibonacci_cache[n] = value
             return value
+
+    @lru_cache(maxsize=None)
+    def fabonacci_using_functoolcache(self, n):
+        # check for integer
+        if not isinstance(n, int):
+            raise TypeError("Please only enter integer")
+        if n < 0:
+            raise ValueError("Kindly Enter positive integer only ")
+        if n < 2:
+            return n
+        return self.fabonacci_using_functoolcache(n - 1) + self.fabonacci_using_functoolcache(n - 2)
 
 
 if __name__ == "__main__":
@@ -81,9 +95,12 @@ if __name__ == "__main__":
     if choice == 2:
         fibonacci.fibonacci_iter(number)
     if choice == 3:
-        for x in range(1,number+1):
-            print(fibonacci.fibonacci_value(x))
-    # if choice == 4:
-    #     fibonacci.
+        for x in range(1, number+1):
+            print(fibonacci.fabonacci_explicit_momization(x))
+    if choice == 4:
+        for x in range(1, number + 1):
+            print(fibonacci.fabonacci_using_functoolcache(x))
+
+
 
 
