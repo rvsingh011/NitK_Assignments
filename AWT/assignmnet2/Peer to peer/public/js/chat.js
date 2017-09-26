7f6f19538d5466ca85abeb308e8aa30f8045514f
Ravi Singh <rvsingh011@gmail.com>
2017-09-26 19:32:01 +0530

    Update till 26/9/2017

--- AWT/assignmnet2/Peer to peer/public/js/chat.js
+++ AWT/assignmnet2/Peer to peer/public/js/chat.js
@@ -1,16 +1,12 @@
-var message = document.getElementById('message');
-var button = document.getElementById('send');
-var username = document.getElementById('username');
-var output = document.getElementById('output');
-var feedback = document.getElementById('feedback');
-var online = document.getElementById('online');
-var caller = document.getElementById('caller');
+var ul = document.getElementById('ul');
 var callButton = document.getElementById('callButton');
-var video = document.querySelector('video');
+var usr = document.getElementById('usr');
+var message = document.getElementById('message');
 var user_info = {};
 var peers = {};
 var socket;
 user_info["username"] = "Ravi";
+
 
 var createPeer = new Promise(function(resolve, reject) {
   var peer = new Peer({
@@ -28,64 +24,43 @@ var createPeer = new Promise(function(resolve, reject) {
 
 
 createPeer.then(function(peer) {
+
   console.log("a New peer connection with id ");
   console.log("creating a new socket");
   socket = io.connect('http://localhost:4000', {
     query: "user=" + JSON.stringify(user_info)
   });
 
-  button.addEventListener('click', function() {
-    socket.emit('chat', {
-      message: message.value,
-      username: username.value
-    });
-  });
-
   // When Call Button is pushed
   callButton.addEventListener('click', () => {
-    console.log(caller.value, peers);
-    var localId = peers[caller.value];
-    console.log(localId);
-    // var conn = peer.connect(localId);
-    // conn.on('open', function() {
-    //   conn.send('hi!');
-    // });
-    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    }, function(stream) {
-        video1.src = window.URL.createObjectURL(stream);
-      }, function(error){
-        console.log("there was an error" + errror);
-      });
+    if (!usr.value){
+      alert("Enter a user");
+    }
+    else if (!message.value) {
+      alert("enter a message");
+    }
+    else{
+      if (peers[usr.value]){
+        var destinationId = peers[usr.value];
+        var conn = peer.connect(destinationId);
+      }
+      else{
+        alert("you entred wrong username");
+      }
+    }
   });
-
-
-
-
-  message.addEventListener('keypress', () => {
-    socket.emit('typing', username.value);
-  });
-
-  //listen for events
-
-  socket.on('chat', (data) => {
-    feedback.innerHTML = "";
-    output.innerHTML += '<p><strong>' + data.username + ' </strong>' + data.message + '</p>';
-  });
-
   socket.on('new_user_added', (data) => {
     peers = JSON.parse(data);
     console.log(peers);
     Object.keys(peers).forEach(function(key) {
-      console.log(key);
-      online.innerHTML += "<h1>" + key + "</h1><br>"
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(key));
+    li.setAttribute("class", "list-group-item list-group-item-success");
+     ul.appendChild(li);
     });
   });
-  socket.on('typing', function(data) {
-    feedback.innerHTML = "<i>" + data + " is Typing</i>......";
-  });
+
+  peer.on('connection', function(comm) { console.log("New user")});
 }, function(error) {
   console.log("there was an error" + error);
 });
